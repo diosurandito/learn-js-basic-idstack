6a542b19e97a5ed4b5887d87baa47d018bd350e8
diosurandito <diosurandito@gmail.com>
2021-03-22 11:11:10 +0700

    episode 00 - 01

---
+++ 01-using-console/app.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello 123';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({a: 1, b: 2});
+console.table({a: 1, b: 2});
+
+console.error('this is some error');
