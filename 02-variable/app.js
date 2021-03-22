c5880120fa92de2756777b2340164dcfc8e599c6
diosurandito <diosurandito@gmail.com>
2021-03-22 12:03:44 +0700

    episode 03-data-types

--- 02-variable/app.js
+++ 02-variable/app.js
@@ -60,7 +60,6 @@ person.age = 28;
 console.log(person);
 
 const numbers = [1,2,3,4,5];
-console.log(numbers);
 numbers.push(6);
 console.log(numbers);
 
---
+++ 03-data-types/app.js
@@ -0,0 +1,39 @@
+// Tipe Data Primitif
+
+// String
+const name = 'John Dio';
+// console.log(typeof name);
+
+// Number
+const age = 23;
+// console.log(typeof age);
+
+// Boolean
+const hasKid = false;
+// console.log(typeof hasKid);
+
+// Null
+const car = null;
+// console.log(typeof car);
+
+
+// Undefined 
+let test;
+console.log(typeof test);
+
+
+// Symbol
+const sym = Symbol();
+
+
+// Tipe Data Referensi
+// Array 
+const hobbies = ['movies', 'music'];
+console.log(hobbies[1]);
+
+// Object
+const address = {
+    city: 'Brebes',
+    zip_code: '52214'
+};
+console.log(address.zip_code);
