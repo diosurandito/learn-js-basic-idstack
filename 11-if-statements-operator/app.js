bb591ac9143c6f5199008a66547b160d0401f200
diosurandito <diosurandito@gmail.com>
2021-03-29 16:26:27 +0700

    episode 11-if-statements-operator

---
+++ 11-if-statements-operator/app.js
@@ -0,0 +1,76 @@
+const id = 100;
+
+// // equal to
+// if (id == 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+    
+// }
+
+// // not equal to
+// if (id != 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+    
+// }
+
+// // equal to value & type
+// if (id === 100) {
+//     console.log('CORRECT');
+    
+// } else {
+//     console.log('INCORRECT');
+    
+// }
+
+// if (id) {
+//     console.log(`ID Value is: ${id}`);
+
+// } else {
+//     console.log(`ID Value is NULL`);
+    
+// }
+
+// if (typeof id != 'undefined') {
+//     console.log(`ID Value is: ${id}`);
+
+// } else {
+//     console.log(`ID Value is undefined`);
+    
+// }
+
+// if (id == 200) {
+//     console.log('CORRECT');
+    
+// }
+
+// // Greater or Less than 
+// if (id >= 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// const color = 'yellow';
+
+// if (color == 'red') {
+//     console.log('Color is red');
+// }else if(color == 'blue') {
+//     console.log('Color is blue');
+// } else {
+//     console.log('Color is not red');
+// }
+
+// Logical Operators
+const name = 'Alpha';
+const age = 20;
+
+if (age > 0 && age < 12) {
+    console.log(`${name} is a child`);
+} else if(age >= 12 && age <= 19){
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`);
+}
