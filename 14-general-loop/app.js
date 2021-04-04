c15befb873050355dc1bf75073329327b94faa4c
diosurandito <diosurandito@gmail.com>
2021-04-04 20:13:02 +0700

    episode 14-general-loop

---
+++ 14-general-loop/app.js
@@ -0,0 +1,32 @@
+// FOR
+
+// for (let i = 0; i < 10; i++) {
+
+//     if (i === 2) {
+//     console.log('2 adalah favorit saya');
+//     continue;
+//     }
+
+//     if (i === 5) {
+//     console.log('stop looping ');
+//     break;
+//     }
+//     console.log('Number ' + i);
+    
+// }
+
+// WHILE
+// let i = 0;
+
+// while(i < 10){
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+
+// DO WHILE
+let i = 0;
+do {
+    console.log('Number ' + i);
+    i++;
+} while (i < 10);
